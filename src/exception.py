96d646e431dcc2ab25e45b932e12e8c28daf0551
Rahul Kendale <rahulkendale21@gmail.com>
2023-03-07 11:46:05 +0200

    syntax error

--- src/exception.py
+++ src/exception.py
@@ -13,7 +13,7 @@ def error_message_details(error,errordetail:sys):
 class CustomException(Exception):
     def __init__(self,error_message,error_details:sys):
         super().__init__(error_message)
-        self.error_message=error_message_details(erro,errordetail=error_details)
+        self.error_message=error_message_details(error,errordetail=error_details)
 
     def __str__(self):
         return self.error_message
